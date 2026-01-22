7f5980eaa8dbf8567c58c3c419750974cd389cf1
tplusk <macfelelhun@gmail.com>
2026-01-22 15:11:46 +0000

    add newRecipe, shoAddRecipe

--- src/components/RecipeManager.jsx
+++ src/components/RecipeManager.jsx
@@ -1,1 +1,8 @@
-export default function RecipeManager() {}
+import React, { useState } from "react";
+
+export default function RecipeManager() {
+  const [newRecipe, setNewRecipe] = useState("");
+  const [showAddRecipe, setShowAddRecipe] = useState(false);
+
+  return <div></div>;
+}
