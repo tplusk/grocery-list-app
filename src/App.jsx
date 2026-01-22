6c46205412a8790f881138c657109f77996d13da
tplusk <macfelelhun@gmail.com>
2026-01-22 16:11:23 +0000

    add handleRecipe, handleDeleteRecipe

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+
+  const handleAddRecipe = (recipe) => {
+    setRecipes([...recipes, recipe]);
+  };
+
+  const handleDeleteRecipe = (recipeName) => {
+    setRecipes(recipes.filter((r) => r !== recipeName));
+    setItems(
+      items.map((item) =>
+        item.recipe === recipeName ? { ...item, recipe: "" } : item,
+      ),
+    );
+  };
+
   return <></>;
 }
 
