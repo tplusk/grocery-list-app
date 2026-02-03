5dc39ad2c36d8f0416905e650ab34d6578980046
tplusk <macfelelhun@gmail.com>
2026-02-03 16:46:56 +0000

    add missing space in front of item

--- src/components/ItemCounter.jsx
+++ src/components/ItemCounter.jsx
@@ -6,7 +6,7 @@ export default function ItemCounter({ count }) {
   return (
     <div className="mt-6 p-4 bg-indigo-50 rounded-lg">
       <p className="text-center text-gray-700">
-        <span className="font-semibold">{count}</span>item
+        <span className="font-semibold">{count}</span> item
         {count !== 1 ? "s" : ""} in your list
       </p>
     </div>
